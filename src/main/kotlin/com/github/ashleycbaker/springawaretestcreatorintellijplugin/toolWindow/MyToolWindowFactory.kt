0861e12a059bcce5e749080ddd7dfa8c374ee73b
GitHub Action <action@github.com>
2023-08-17 21:56:40 +0000

    Template cleanup

--- src/main/kotlin/com/github/ashleycbaker/springawaretestcreatorintellijplugin/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/ashleycbaker/springawaretestcreatorintellijplugin/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.ashleycbaker.springawaretestcreatorintellijplugin.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.ashleycbaker.springawaretestcreatorintellijplugin.MyBundle
+import com.github.ashleycbaker.springawaretestcreatorintellijplugin.services.MyProjectService
 import javax.swing.JButton
 
 
